bfc3eb2245b2bcae1ce78e63d3a01b71d1b17c16
chintan <chintanpatel1081@gmail.com>
2025-12-15 21:44:26 +0530

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import LandingPage from "./components/LandingPage"
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
