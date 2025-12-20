48bcd31ed071783cf56d9f66b55c47ed5004b5c7
chintan <chintanpatel1081@gmail.com>
2025-12-20 21:54:09 +0530

    added footer section

---
+++ src/components/FooterSection.tsx
@@ -0,0 +1,13 @@
+const FooterSection = () => {
+  return (
+    <div>
+      <div className="mx-auto max-w-2xl border-x border-neutral-200">
+        <div className="p-8 text-center text-neutral-400 text-xs font-medium">
+            Design & Developed by Chintan Patel
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FooterSection
